9bc9bd44f6a452692dbfeac758e672fbf05fcea2
Gustavo Stacoviaki Grassi <106496948+grassiii@users.noreply.github.com>
2025-11-12 11:20:09 -0300

    mudar para tema biblioteca e adicionar busca

---
+++ biblioteca_livros/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+class Pessoa(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    def __repr__(self):
+        return f'<Pessoa {self.nome}>'
